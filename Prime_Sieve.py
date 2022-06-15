248ba52f7d77dc79a083447a65ab90891348ca20
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-06-15 18:22:02 +0900

    Update Prime_Sieve.py

--- Prime_Sieve.py
+++ Prime_Sieve.py
@@ -1,16 +1,13 @@
 def prime_Sieve(n):
-    
     sieve = [True] * n                       # 태초에 모든 칸은 True(소수)로 판단
     
-    m = int(n ** 0.5)                        # 소수가 아니라면 약수가 2와 sqrt(N) 사이에 있다.
-    for i in range(4, n, 2):
-        sieve[i] = False                     # 2가 아닌 2의 배수를 전부 False(합성수)로 판단
-        
+    m = int(n ** 0.5)                        # 소수가 아니라면 N의 약수가 2와 sqrt(N) 사이에 있다.
+    #for i in range(4, n, 2):
+    #    sieve[i] = False                    # 2가 아닌 2의 배수를 전부 False(합성수)로 판단...하지도 않고 아예 무시한다.
     for i in range(9, n, 6):
         sieve[i] = False                     # 3이 아닌 3의 배수를 전부 False(합성수)로 판단
     
     for i in range(3, m + 1, 2):             # i는 3부터 홀수만 따짐
-        
         if sieve[i] == True:                 # i가 소수 일때는
             for j in range(5 * i, n, 6 * i): # i가 아닌 i의 배수들을 전부 False(합성수)로 판단
                 sieve[j] = False
